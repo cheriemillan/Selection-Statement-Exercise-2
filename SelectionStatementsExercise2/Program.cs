baad9f98562101a5be471084e96318d329c413f5
CherieMillan <cherie99millan@gmail.com>
2024-07-11 12:06:46 -0400

    Made a switch statement with different cases to ask the user what is their favorite subject

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string userInput = Console.ReadLine();
+
+            switch (userInput.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I like math a lot too!");
+                    break;
+                case "english":
+                    Console.WriteLine("I like english too, but I hate the grammar part :(");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is cool, but hard!");
+                    break;
+                case "history":
+                    Console.WriteLine("I'm not a big fan of history, but I'm good at it too which is weird?");
+                    break;
+                case "art":
+                    Console.WriteLine("I'm terrible at drawing, but at least you like it!");
+                    break;
+                default:
+                    Console.WriteLine("That is not a chosen school subject or you may not like any subject which is fine too!");
+                    break;
+            }
+           
+                
+                
+
         }
     }
 }
